f5df4ed1f29ec977df09d12926ce2428547aa915
Lori2018 <lcb20010@gmail.com>
2020-02-21 22:29:42 -0500

    commit

--- Tyranus/src/main/java/frc/robot/Constants.java
+++ Tyranus/src/main/java/frc/robot/Constants.java
@@ -82,6 +82,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kSecondControllerPort = 1;
 
   }
 
--- Tyranus/src/main/java/frc/robot/RobotContainer.java
+++ Tyranus/src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import java.util.List;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
@@ -21,6 +22,7 @@ import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -28,6 +30,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.SwerveDriveConstants;
 import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.ControlPanelSubsystem;
 import frc.robot.subsystems.SwerveDrive;
 
 /*
@@ -38,10 +41,12 @@ import frc.robot.subsystems.SwerveDrive;
  */
 public class RobotContainer {
   // The robot's subsystems
-  private final SwerveDrive swerveDrive = new SwerveDrive();
+  // private final SwerveDrive swerveDrive = new SwerveDrive();
+  private final ControlPanelSubsystem controlPanel = new ControlPanelSubsystem();
 
   // The driver's controller
   Joystick m_driverController = new Joystick(OIConstants.kDriverControllerPort);
+  Joystick m_secondController = new Joystick(OIConstants.kSecondControllerPort);
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -52,13 +57,13 @@ public class RobotContainer {
 
     // Configure default commands
     // Set the default drive command to split-stick arcade drive
-    swerveDrive.setDefaultCommand(
-        // A split-stick arcade command, with forward/backward controlled by the left
-        // hand, and turning controlled by the right.
-        new RunCommand(() -> swerveDrive.drive(
-            m_driverController.getRawAxis(1),
-            m_driverController.getRawAxis(0),
-            m_driverController.getRawAxis(4), false), swerveDrive));
+    // swerveDrive.setDefaultCommand(
+    //     // A split-stick arcade command, with forward/backward controlled by the left
+    //     // hand, and turning controlled by the right.
+    //     new RunCommand(() -> swerveDrive.drive(
+    //         m_driverController.getRawAxis(1),
+    //         m_driverController.getRawAxis(0),
+    //         m_driverController.getRawAxis(4), false), swerveDrive));
 
   }
 
@@ -69,6 +74,8 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
+    new JoystickButton(m_secondController, Button.kB.value)
+         .whenPressed(new InstantCommand(controlPanel::displayCurrentColor, controlPanel));
   }
 
 
@@ -77,46 +84,46 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // Create config for trajectory
-    TrajectoryConfig config =
-        new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
-                             AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(SwerveDriveConstants.kDriveKinematics);
+  // public Command getAutonomousCommand() {
+  //   // Create config for trajectory
+  //   TrajectoryConfig config =
+  //       new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+  //                            AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+  //           // Add kinematics to ensure max speed is actually obeyed
+  //           .setKinematics(SwerveDriveConstants.kDriveKinematics);
 
     // An example trajectory to follow.  All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(
-            new Translation2d(1, 1),
-            new Translation2d(2, -1)
-        ),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
-        config
-    );
+    // Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+    //     // Start at the origin facing the +X direction
+    //     new Pose2d(0, 0, new Rotation2d(0)),
+    //     // Pass through these two interior waypoints, making an 's' curve path
+    //     List.of(
+    //         new Translation2d(1, 1),
+    //         new Translation2d(2, -1)
+    //     ),
+    //     // End 3 meters straight ahead of where we started, facing forward
+    //     new Pose2d(3, 0, new Rotation2d(0)),
+    //     config
+    // );
 
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-        exampleTrajectory,
-        swerveDrive::getPose, //Functional interface to feed supplier
-        SwerveDriveConstants.kDriveKinematics,
+    // SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+    //     exampleTrajectory,
+    //     swerveDrive::getPose, //Functional interface to feed supplier
+    //     SwerveDriveConstants.kDriveKinematics,
 
-        //Position controllers
-        new PIDController(AutoConstants.kPXController, 0, 0),
-        new PIDController(AutoConstants.kPYController, 0, 0),
-        new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0,
-                                  AutoConstants.kThetaControllerConstraints),
+    //     //Position controllers
+    //     new PIDController(AutoConstants.kPXController, 0, 0),
+    //     new PIDController(AutoConstants.kPYController, 0, 0),
+    //     new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0,
+    //                               AutoConstants.kThetaControllerConstraints),
 
-        swerveDrive::setModuleStates,
+    //     swerveDrive::setModuleStates,
 
-        swerveDrive
+    //     swerveDrive
 
-    );
+    // );
 
     // Run path following command, then stop at the end.
-    return swerveControllerCommand.andThen(() -> swerveDrive.drive(0, 0, 0, false));
-  }
+    // return swerveControllerCommand.andThen(() -> swerveDrive.drive(0, 0, 0, false));
+//  }
 }
